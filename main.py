631c54ff9e4db5fb2f4063fcd04d539d0f82750a
unknown <developer.mattklaus@gmail.com>
2023-08-18 18:12:46 +0100

    track progress :truck:

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import re
 import logging
 import datetime
 import colorlog
+import threading
 
 # Create a logger
 logger = logging.getLogger(__name__)
@@ -50,6 +51,28 @@ def test_database_connection(database, username, password, host="localhost", por
         return False
 
 
+def read_progress(progress_file):
+    path = os.path.join(os.getcwd(), progress_file)
+    if progress_file and os.path.isfile(path):
+        with open(path, 'r') as file:
+            data = file.readline()
+            if len(data) and data.isdigit():
+                return int(data)
+    
+    return False
+
+
+def write_progress(progress_file, progress):
+    if progress_file and len(str(progress)):
+        path = os.path.join(os.getcwd(), progress_file)
+        with open(path, "w") as file:
+            file.write(str(progress))
+        return True
+    
+    return False
+
+
+
 def extract_pending_number(file_name):
     match = re.search(r"_(\d+)\.sql$", file_name)
     if match:
@@ -57,23 +80,37 @@ def extract_pending_number(file_name):
     return 0
 
 
-def migrate_data(sql_files_dir, database_name, username, password):
+def migrate_data(sql_files_dir, database_name, username, password, progress_file):
     # Get a list of SQL files in the directory
     sql_files = glob.glob(os.path.join(sql_files_dir, "*.sql"))
     sql_files.sort(key=extract_pending_number)
     files_len = len(sql_files)
 
-    # Iterate over the files and import them one by one
-    for index, sql_file in enumerate(sql_files):
+    progress = read_progress(progress_file)
+    resume = False
+
+    if progress:
+        yn = input(f"Resume after {progress + 1}/{files_len} ({sql_files[progress]})? (Enter: y for 'Yes' or n for 'No'): ")
+        if yn.startswith("y") or yn.startswith("Y"):
+            resume = True
+
+    # Iterate over the files and import them one by one. If resume is True, skip up to the nth file
+    for index, _ in enumerate(sql_files, start=(progress if resume else 0)):
+        sql_file = sql_files[index]
         # Execute the MySQL command to import the data
         cmd = f"mysql -u{username} --password={password} -D{database_name} < {sql_file}" # cmd = f"mysql -uroot --password= -D{database_name} < {sql_file}"
         result = subprocess.run(cmd, shell=True)
 
         if result.returncode != 0:
-            logger.warning(f"{index + 1}/{files_len} Failed to migrate data from SQL file {sql_file}")
+            logger.warning(f"{index + 1}/{files_len} Error migrating file {os.path.basename(sql_file)}")
 
         else:
-            logger.info(f"{index + 1}/{files_len} Data migration completed for file: {sql_file}")
+            # Register progress of migration as a thread
+            t = threading.Thread(target=write_progress, args=(progress_file, index))
+            t.start()
+            t.join()
+
+            logger.info(f"{index + 1}/{files_len} Successfully executed: {os.path.basename(sql_file)}")
 
 def main():
 
@@ -84,6 +121,7 @@ def main():
     parser.add_argument("--password", default="", help="MySQL password.")
     parser.add_argument("--host", default='localhost', help="MySQL host.")
     parser.add_argument("--port", default=3306, help="MySQL port.")
+    parser.add_argument("--progress-file", default="counter.txt", help="Text file to track progress.")
 
     args = parser.parse_args()
 
@@ -91,7 +129,10 @@ def main():
     if not test_database_connection(args.database, args.username, args.password, args.host, args.port):
         return
 
-    migrate_data(args.sql_dir, args.database, args.username, args.password)
+    migrate_data(args.sql_dir, args.database, args.username, args.password, args.progress_file)
+
+    # Migration complete
+    write_progress(args.progress_file, "")
 
     logger.info("All data migrations completed successfully.")
 
